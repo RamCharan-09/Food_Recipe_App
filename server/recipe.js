d0043ece43dce916419ee74d1efbf17c69f8ac0c
RamCharan-09 <ramcharanbarma99@gmail.com>
2024-07-02 17:19:13 +0530

    Updated HTTP api methods

---
+++ server/recipe.js
@@ -0,0 +1,32 @@
+//Basically defining the schema
+
+const mongoose = require('mongoose');
+const recipeSchema = new mongoose.Schema ({
+    dishName: {
+        type: String,
+        required: [true, 'Dish name is required']
+    },
+    timeToPrepare: {
+        type: String,
+        required: true
+    },
+    ingredients: {
+        type: [String],
+        required: true
+    },
+    steps: {
+        type: String,
+        required: true
+    },
+    author: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = mongoose.model("recipe", recipeSchema)
+
